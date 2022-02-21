51c9ac4777c1ffa35dd0081baca383fe80647f04
pariosa <ariosa@gmail.com>
2022-02-20 19:24:32 -0500

    css changes

--- pages/index.js
+++ pages/index.js
@@ -8,65 +8,70 @@ const Container = Styled.body`
   width: 100%;
   min-width:100%;
   display: grid;
-  grid-template-columns:repeat(2, 100%);
-  height:100vh;
+  grid-template-columns:repeat(2, 100%); 
+  height: calc(var(--vh, 1vh) * 100);
 `;
 const Title = Styled.div`
   font-family: 'Libre Barcode 128 Text', cursive;
   font-size: ${fontSizes.h1};
   display:flex;
-  width:100%;
-  height:100vh;
+  width:100%; 
+  align-items:space-around;
+  height: calc(var(--vh, 1vh) * 100);
   flex-direction: row;
     * {
       cursor:pointer;
       display: flex;
       align-items: center;
+
       flex-direction:row;
       height:100vh;
     }
 `;
 const Black = Styled.div` 
   text-align:right;
-  background-color: ${colors.black};
-  color: ${colors.white}; 
+  
+  background-color: ${colors.white};
+  color: ${colors.black}; 
 `;
 
 const Spacer = Styled.div`
-  min-width: 35%;
+  min-width: 15%;
+  background-color: ${colors.white};
   max-width: 35%;
   flex: 1; 
 `;
 const BlackSpacer = Styled.div`
-  min-width: 35%;
-  max-width: 35%;
+  min-width: 15%;
+  max-width: 43%;
   flex: 1;
   background-color: ${colors.black};
 `;
 const White = Styled.div`
   flex-basis: fill;  
-  background-color: ${colors.white};
-  color: ${colors.black}; 
+  
+  background-color: ${colors.black};
+  color: ${colors.white}; 
 `;
 
 export default function Home({ isConnected }) {
   return (
-  <Container>
-    <Title>
-      <BlackSpacer />
+    <>
       <NextLink passHref href="https://www.facebook.com/dvrksydvrstalent/">
-          <Black>
-            DVRK
-          </Black>
-        </NextLink>      
-        <NextLink passHref href="https://www.facebook.com/dvrksydvrstalent/">
-          <White>
-            SYDERS
-          </White>
-        </NextLink>
-      <Spacer/>
-    </Title>
-  </Container>
+        <Container>
+          <Title>
+            <BlackSpacer />
+                <Black>
+                  DVRK
+                </Black>
+                <White>
+                  SYDERS
+                </White> 
+            <Spacer/>
+          </Title>
+        </Container>
+      </NextLink>
+    </>
   )
 }
 
